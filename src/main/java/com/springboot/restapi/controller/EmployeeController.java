d0c35cda760cfbd32fbe813a73adec94d1d9b36b
souravsaket31 <souravsaket31@gmail.com>
2024-07-29 20:17:33 +0530

    refactoring - added logs, moved the logic to service class

--- src/main/java/com/springboot/restapi/controller/EmployeeController.java
+++ src/main/java/com/springboot/restapi/controller/EmployeeController.java
@@ -1,29 +1,36 @@
 package com.springboot.restapi.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 
-
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.springboot.restapi.exception.ResourceNotFoundException;
 import com.springboot.restapi.model.Employee;
 import com.springboot.restapi.repository.EmployeeRepository;
+import com.springboot.restapi.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
 
-	static List<Employee> empList = new ArrayList<>();
 	@Autowired
 	EmployeeRepository employeeRepository;
+
+	@Autowired
+	EmployeeService employeeService;
+
+	private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
 
 	// 1. get all
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees() {
+		logger.info("Fetching all employees from h2 db");
 		return employeeRepository.findAll();
 	}
 
@@ -38,6 +45,7 @@ public class EmployeeController {
 	 * */
 	@PostMapping("/employees")
 	public Employee createEmployee(@RequestBody Employee employee) {
+		logger.info("saved employee data in h2 db {}", employee.toString());
 		return employeeRepository.save(employee);
 	}
 
@@ -48,6 +56,8 @@ public class EmployeeController {
 			throws ResourceNotFoundException {
 		Employee employee = employeeRepository.findById(empId)
 				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + empId));
+
+		logger.info("fetching employee by id {}", empId);
 		return ResponseEntity.ok().body(employee);
 	}
 
@@ -63,6 +73,7 @@ public class EmployeeController {
 		employee.setLastName(employeeDetails.getLastName());
 
 		final Employee updatedEmployee = employeeRepository.save(employee);
+		logger.info("updating employee by id {}", empId);
 
 		return ResponseEntity.ok(updatedEmployee);
 	}
@@ -74,23 +85,22 @@ public class EmployeeController {
 		employeeRepository.findById(empId)
 				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + empId));
 		employeeRepository.deleteById(empId);
+		logger.info("deleting employee by id {}", empId);
 		return ResponseEntity.ok().build();
 	}
 
 	@PostMapping("/load")
-	public ResponseEntity<List<Employee>> loadAllEmployees() {
-		IntStream.range(0, 100).forEach(i -> {
-			empList.add(new Employee(i, "FirstName-" + i, "LastName-" + i, "email" + i + "@example.com"));
-		});
-
-	//	employeeRepository.saveAll(empList);
-		return new ResponseEntity<>(empList, HttpStatus.CREATED);
+	public ResponseEntity<List<Employee>> staticLoadEmployees() {
+		List<Employee> employees = employeeService.loadEmployees();
+		logger.info("Storing static list of employees");
+		return new ResponseEntity<>(employees, HttpStatus.CREATED);
 	}
-	@GetMapping("/getAll")
-	public ResponseEntity<List<Employee>> getPreLoadedEmployees() {
-		//List<Employee> all = employeeRepository.findAll();
 
-		return new ResponseEntity<>(empList, HttpStatus.OK);
+	@GetMapping("/static/employees")
+	public ResponseEntity<List<Employee>> getStaticListOfEmployees() {
+		List<Employee> employees = employeeService.getStaticEmployees();
+		logger.info("Fetching all static list of employees");
+		return new ResponseEntity<>(employees, HttpStatus.OK);
 	}
 
 
---
+++ src/main/java/com/springboot/restapi/service/EmployeeService.java
@@ -0,0 +1,25 @@
+package com.springboot.restapi.service;
+
+import com.springboot.restapi.model.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Service
+public class EmployeeService {
+
+    private List<Employee> staticEmployees = new ArrayList<>();
+
+    public List<Employee> loadEmployees() {
+        IntStream.range(0, 100).forEach(i -> {
+            staticEmployees.add(new Employee(i, "FirstName-" + i, "LastName-" + i, "email" + i + "@example.com"));
+        });
+        return staticEmployees;
+    }
+
+    public List<Employee> getStaticEmployees() {
+        return staticEmployees;
+    }
+}
